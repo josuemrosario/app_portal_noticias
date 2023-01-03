e905d00b4b14cb1290778f89bffba85415c339c2
josue Micaroni do Rosario <josuemrosario@yahoo.com.br>
2023-01-03 15:54:59 -0300

    Secao 20 - Aula 399 (Migration created but not executed)

---
+++ Scripts_criacao_bd.sql
@@ -0,0 +1,12 @@
+/*=======================================
+// Projeto: Portal de Noticias
+// Descricao:  Projeto de portal de noticias usando Laravel e Redis
+=======================================*/
+
+
+/*=======================================
+// Criação do database e use
+=======================================*/
+
+CREATE DATABASE pn;
+USE pn;
